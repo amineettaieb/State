3b9de96936f42b6f3df0810138b04a2161b63b52
amineettaieb <amine.ettaieb@gmail.com>
2022-10-03 11:26:37 +0100

    state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
+  state = {
+    Person: {
+      fullName: 'Amin'
+      , bio: 'hello',
+      imgSrc: '',
+      profession: 'web developer',
+    },
+    Show: true,
+    Elapsed: 0,
+  }
+  show = () => {
+    this.setState({ Show: !this.state.Show })
+    console.log(this.state.Show)
+  }
 
-export default App;
+  componentDidMount() {
+    this.interval = setInterval(()=>this.setState(s=>({Elapsed: s.Elapsed + 1})), 1000)
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.interval)
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        {/* <img src={this.Person.}/> */}
+        <h3>{this.state.Elapsed}</h3>
+        {this.state.Show && <div>
+          <h1>{this.state.Person.fullName}</h1>
+          <h2>{this.state.Person.bio}</h2>
+          <h3>{this.state.Person.profession}</h3>
+        </div>}
+        <button onClick={this.show}>{this.state.Show ? 'Hide' : 'Show'}</button>
+      </div>
+    );
+  }
+
+}
